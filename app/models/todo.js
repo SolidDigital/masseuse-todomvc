1406910d06b64680ddafaa83325dc89ca91dda4c
Peter Ajtai <peter.ajtai@thinksolid.com>
2014-02-17 08:02:17 -0800

    adding dom field to models, and hiding items left if there are zero

--- app/models/todo.js
+++ app/models/todo.js
@@ -23,6 +23,7 @@ define([
 
     function initialize() {
         var channels = new masseuse.utilities.channels();
+        this.dom = {};
         this.listenTo(channels, 'filter', this.setVisibility);
         this.on('change:completed change:title', function() {
             this.save();
@@ -30,22 +31,21 @@ define([
     }
 
     function close() {
-        this.set('editing', false);
-        this.unset('autofocus');
+        this.dom.editing = false;
+        this.dom.autofocus = undefined;
         this.save();
     }
 
     function editing() {
-        console.log('editing');
-        this.set('editing', true);
-        this.set('autofocus', true);
+        console.log(arguments);
+        this.dom.editing = true;
+        this.dom.autofocus = 'autofocus';
     }
 
     function setVisibility(filter) {
-        var isCompleted = this.get('completed'),
-            isHidden = (!isCompleted && filter === 'completed') ||
-                (isCompleted && filter === 'active');
-        this.set('isHidden', isHidden);
-        this.set('filter',filter);
+        var isCompleted = this.get('completed');
+        this.dom.isHidden = (!isCompleted && filter === 'completed') ||
+            (isCompleted && filter === 'active');
+        this.dom.filter = filter;
     }
 });
